d1f3e150ea36855668fbe52ab99b5b4f4906301a
zzxzzg <xiaobanma2292@gmail.com>
2016-12-28 23:45:43 +0800

    add first script

---
+++ Assets/_Scripts/SwanController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwanController : MonoBehaviour {
+    Rigidbody2D rigidBody;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
